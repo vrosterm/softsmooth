1bc20538b3451d6d7fc4caf0ece887d3672ec9ac
vrosterm <vrosterm@calpoly.edu>
2025-07-09 14:58:39 -0700

    Copied ME_400 CODE.py to train.py and altered its layout. This will be our code that we work on.

---
+++ S25_BASECODE/train.py
@@ -0,0 +1,123 @@
+from __future__ import print_function
+import os, argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=1, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training (use CPU)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+
+# Device configuration
+device = torch.device("cuda" if not args.no_cuda and torch.cuda.is_available() else "cpu")
+
+torch.manual_seed(args.seed)
+
+# print('===> Building model')
+# We can swap this out with relevant models as we decide to change things.
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv2_drop = nn.Dropout2d(p=0.5)
+        self.fc1 = nn.Linear(1024, 128)
+        self.fc2 = nn.Linear(128, 10)
+    
+    """ Unsure if needed, commenting out for now.
+    def forward(self, x):
+        x = F.leaky_relu(self.bn1(self.conv1(x)), negative_slope=0.01)
+        x = F.max_pool2d(x, 2)
+        x = F.leaky_relu(self.bn2(self.conv2_drop(self.conv2(x))), negative_slope=0.01)
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = F.leaky_relu(self.fc1(x), negative_slope=0.01)
+        x = F.dropout(x, p=0.5)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+    """
+
+model = Net().to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.1f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+if __name__ == "__main))":
+    print('===> Loading data')
+    train_transforms = transforms.Compose([
+        transforms.RandomRotation(10),
+        transforms.RandomAffine(0, shear=10, scale=(0.8, 1.2)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    train_loader = DataLoader(
+        datasets.MNIST('data', train=True, download=True, transform=train_transforms),
+        batch_size=args.batch_size, shuffle=True
+    )
+    test_loader = DataLoader(
+        datasets.MNIST('data', train=False, transform=test_transforms),
+        batch_size=args.test_batch_size, shuffle=False
+    )
+    
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        test()
+        scheduler.step()
