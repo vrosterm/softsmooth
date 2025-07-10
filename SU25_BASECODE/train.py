7ef9f120bd699a859cee6aef9201a53a836d10fd
vrosterm <vrosterm@calpoly.edu>
2025-07-10 00:35:09 -0700

    smooth function is started. Not implemented yet.

--- SU25_BASECODE/train.py
+++ SU25_BASECODE/train.py
@@ -38,12 +38,21 @@ def epoch(loader, model, opt=None):
         total_loss += loss.item() * X.shape[0]
     return total_err / len(loader.dataset), total_loss / len(loader.dataset)
 
+### Returns average of all scores for one input. I'm not sure what x should be in this case, either the batch or one of the inputs.
+def smooth(x: torch.tensor, model: torch.nn.Module, sigma: float, n_samples: int): 
+    batch = x.repeat((batch_size, 1)) #1 channel, since B&W
+    epsilon = sigma * torch.randn(batch, n_samples)
+    scores = model.forward(batch + epsilon)
+    avg_scores = torch.mean(scores, dim = 1)
+    return torch.argmax(avg_scores)
+
 if __name__ == "__main__":
     # The following only uses model_dnn_2. At later points we can add an arg that loads a model for us.
+    batch_size = 100
     mnist_train = datasets.MNIST("../data", train=True, download=True, transform=transforms.ToTensor())
     mnist_test = datasets.MNIST("../data", train=False, download=True, transform=transforms.ToTensor())
-    train_loader = DataLoader(mnist_train, batch_size = 100, shuffle=True)
-    test_loader = DataLoader(mnist_test, batch_size = 100, shuffle=False)
+    train_loader = DataLoader(mnist_train, batch_size = batch_size, shuffle=True)
+    test_loader = DataLoader(mnist_test, batch_size = batch_size, shuffle=False)
     opt = optim.SGD(model_dnn_2.parameters(), lr=1e-1)
     print('train_err\ttrain_loss\ttest_err\ttest_loss')
     for _ in range(10):
