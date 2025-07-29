2441581abc5db0a6541134e9864c39fe809cf56f
faithb12890 <faithbergstrom@gmail.com>
2025-07-29 15:14:47 -0700

    Created combined mu and sigma neural net and updated training code to match new network. No learnable bias yet.

--- SU25_IDRS/IDRS_params.py
+++ SU25_IDRS/IDRS_params.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 import os
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
@@ -25,6 +26,22 @@ class Bias(nn.Module):
         self.bias = nn.Parameter(torch.ones(1)*0.1)
     def forward(self, x):
         return x.add(self.bias)
+    
+class ReLUBias(nn.Module):
+    '''ReLU and Bias layer applied to only the second half of the neural net
+    (the half with sigma)'''
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.bias = nn.Parameter(torch.ones(1)*0.1)
+    def forward(self, x):
+        # In our current mu/sigma network, x is a FloatTensor of shape (100,1568).
+        L = len(x[0])//2
+        sigma_vals = x[:,L:]
+        sigma_vals = F.relu(sigma_vals) # ReLU
+        sigma_vals += 0.1               # Bias
+        x[:,L:] = sigma_vals
+        return x
 
 model_sigma = nn.Sequential(
     Flatten(), nn.Linear(784,200), nn.ReLU(), 
@@ -40,6 +57,13 @@ model_mu = nn.Sequential(
     nn.Linear(100,784)
 ).to(device)
 
+model_mu_sig = nn.Sequential(
+    Flatten(), nn.Linear(784,200), nn.ReLU(), 
+    nn.Linear(200,100), nn.ReLU(),
+    nn.Linear(100,100), nn.ReLU(),
+    nn.Linear(100,1568), ReLUBias()
+).to(device)
+
 # Data
 mnist_train = datasets.MNIST("../data", train=True, download=True, transform=transforms.ToTensor())
 mnist_test = datasets.MNIST("../data", train=False, download=True, transform=transforms.ToTensor())
@@ -51,21 +75,25 @@ model_dnn_2 = nn.Sequential(Flatten(), nn.Linear(784,200), nn.ReLU(),
                                 nn.Linear(200,10)).to(device)
 model_dnn_2.load_state_dict(torch.load("model_dnn_2.pt", map_location=device, weights_only=True))
 
-def epoch_params(pretrained, model_sigma, model_mu, loader, *args, lam=0.3, L=1.0,):
+def epoch_params(pretrained, model_params, loader, *args, lam=0.3, L=1.0,):
     '''Learns the sigma and mu neural nets. Incomplete.'''
     total_loss, total_err = 0.,0.
     for X,y in loader:
-        # Getting initial X, y, sigma, mu tensors
+        # Getting initial X, y, output tensors
         X,y = X.to(device), y.to(device) # X is shape (100,1,28,28)- batch of images
-        sigma = model_sigma(X)  # (100, 784) tensor- X is (100,1,28,28)
+        outputs = model_params(X)  # (100, 1568) tensor- X is (100,1,28,28)
+        
+        # Organizing mu from outputs. Means are the first 784 values.
+        mu = outputs[:,:(len(outputs[0])//2)] # (100, 784) tensor
+        mu = mu.detach().cpu().numpy()
+        
+        #Organizing sigma from outputs. Sigma is the second 784 values.
+        sigma = outputs[:,(len(outputs[0])//2):] # (100, 784) tensor
         sigma_diag = np.zeros((len(sigma),len(sigma[0]),len(sigma[0])))
         sigma_diag[np.arange(len(sigma))[:, None], np.arange(len(sigma[0])), np.arange(len(sigma[0]))] = sigma.detach().cpu().numpy()
         # Squaring the given sigma^1/2 matrices
         sigma_diag = np.matmul(sigma_diag,sigma_diag)
-
-        mu = model_mu(X)        # (100, 784) tensor
-        mu = mu.detach().cpu().numpy()
-
+        
         # Calling new randomized smoothing function. g is the top 2 items, yp is predicted labels.
         g, yp = IDRS_matrices(pretrained, mu, sigma_diag, X, n_samples=50)
                
@@ -87,39 +115,29 @@ def epoch_params(pretrained, model_sigma, model_mu, loader, *args, lam=0.3, L=1.
                 spec_reg += spec_norm
 
         loss += lam * spec_reg
-        num_linear_layers = sum(1 for layer in model_sigma if isinstance(layer, nn.Linear)) # Will want to make this the layers in the combined mu/sigma model
+        num_linear_layers = sum(1 for layer in model_params if isinstance(layer, nn.Linear)) # Will want to make this the layers in the combined mu/sigma model
         L_const = L ** (1 / num_linear_layers) # We might want to feed our chosen L into the function parameters
         
         if opt:
             opt.zero_grad()
             loss.backward()
             opt.step()
-
-            # Rewrite below once mu and sigma models are a single model
-            spectral_sigma = []
-            spectral_mu = []
+            
+            spectral_norms = []
             
             #New weight normalization step without parsing through parameter names
-            for layer in model_sigma:
+            for layer in model_params:
                 if isinstance(layer, nn.Linear):
                     weight = layer.weight.data
                     spec_norm = torch.linalg.matrix_norm(weight)
-                    spectral_sigma.append(spec_norm.item())
+                    spectral_norms.append(spec_norm.item())
                     norm_weight = L_const * weight / spec_norm
                     layer.weight.data.copy_(norm_weight)
-
-            for layer in model_mu:
-                if isinstance(layer, nn.Linear):
-                    weight = layer.weight.data
-                    spec_norm = torch.linalg.matrix_norm(weight)
-                    spectral_mu.append(spec_norm.item())
-                    norm_weight = L_const * weight / spec_norm
-                    layer.weight.data.copy_(norm_weight)
-
         
         yp_tensor = torch.tensor(yp, device=y.device)
         total_err += (yp_tensor != y).sum().item()
-        total_loss += loss.item() * X.shape[0] # Add on lambda*sum(spectralnorms). We have norms for both mu and sigma.
+        total_loss += loss.item() * X.shape[0]
+        raise KeyboardInterrupt
     return total_err / len(loader.dataset), total_loss / len(loader.dataset)
 
 # Copied from train_save_smooth
@@ -132,5 +150,5 @@ num_iter = 40 # Number of iterations
 if not os.path.exists("model_IDRS.pt"):
     opt = optim.SGD(model_dnn_2.parameters(), lr=0.1)
     for _ in range(10):
-        epoch_params(model_dnn_2, model_sigma, model_mu, train_loader, training_epsilon, alpha, num_iter, lam=0.3, L=1.0)
+        epoch_params(model_dnn_2, model_mu_sig, train_loader, training_epsilon, alpha, num_iter, lam=0.3, L=1.0)
     torch.save(model_dnn_2.state_dict(), "model_IDRS.pt")
