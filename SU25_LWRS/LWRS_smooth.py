d09b1fed261ab852106a9604dd8ad5fe2631e462
vrosterm <vrosterm@calpoly.edu>
2025-07-30 14:11:31 -0700

    Change to smooth script to allow for dnn 4 testing

--- SU25_LWRS/LWRS_smooth.py
+++ SU25_LWRS/LWRS_smooth.py
@@ -9,9 +9,9 @@ import os
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import random
-from LWRS_model import LWRS
+from LWRS_model import LWRS, DNN_4
 from tqdm import tqdm
-from SU25_BASECODE.train_save_smooth import epoch, epoch_adversarial, pgd_linf, evaluate_clean, evaluate_under_attack, phi_inverse
+from SU25_BASECODE.train_save_smooth import epoch, epoch_adversarial, pgd_linf, pgd_l2, evaluate_clean, evaluate_under_l2
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -22,34 +22,39 @@ train_loader = DataLoader(mnist_train, batch_size=100, shuffle=True)
 test_loader = DataLoader(mnist_test, batch_size=100, shuffle=False)
 
 # PGD attack parameters
-epsilon = 0.082  # Maximum perturbation
+epsilon = 1  # Maximum perturbation
 alpha = 0.01 # Step size
 num_iter = 40 # Number of iterations
 
 LWRS_model = LWRS().to(device)
 opt_LWRS = optim.SGD(LWRS_model.parameters(), lr=0.1)
 
+dnn4 = DNN_4().to(device)
+
+dnn4_clean = DNN_4(noise_std=0).to(device)
+opt_dnn4_clean = optim.SGD(dnn4_clean.parameters(), lr=0.1)
+
 class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.shape[0], -1)
 
-if __name__ == '__main__': 
-    if not os.path.exists("models/LWRS_model_test.pt"):
-        for epoch_num in range(10):
-            print(f"Epoch {epoch_num+1}:")
-            train_err, train_loss = epoch(train_loader, LWRS_model, opt_LWRS)
-            test_err, test_loss = epoch(test_loader, LWRS_model, opt_LWRS)
-            adv_err, adv_loss = epoch_adversarial(test_loader, LWRS_model, pgd_linf, opt_LWRS, epsilon, alpha, num_iter)
-            print("| Train Accuracy:     {:.2f}%".format(100 - train_err * 100))
-            print("| Test Accuracy:      {:.2f}%".format(100 - test_err * 100))
-            print("| Adversarial Accuracy:  {:.2f}%".format(100 - adv_err * 100))
-        torch.save(LWRS_model.state_dict(), "models/LWRS_model_test.pt")
+# Training and Evaluation
+if not os.path.exists("SU25_LWRS/models/dnn_4_l2e1_test.pt"):
+    for epoch_num in range(10):
+        print(f"Epoch {epoch_num+1}:")
+        train_err, train_loss = epoch_adversarial(dnn4_clean, train_loader, pgd_l2, opt_dnn4_clean, epsilon, alpha, num_iter)
+        # test_err, test_loss = epoch(test_loader, LWRS_model)
+        # adv_err, adv_loss = epoch_adversarial(test_loader, LWRS_model, pgd_linf, epsilon, alpha, num_iter)
+        print("| Train Accuracy:     {:.2f}%".format(100 - train_err * 100))
+        # print("| Test Accuracy:      {:.2f}%".format(100 - test_err * 100))
+        # print("| Adversarial Accuracy:  {:.2f}%".format(100 - adv_err * 100))
+    torch.save(dnn4_clean.state_dict(), "SU25_LWRS/models/dnn_4_l2e1_test.pt")
 
-    # Load saved LWRS model
-    LWRS_model.load_state_dict(torch.load("models/LWRS_model_test.pt", map_location=device))
+# Load saved LWRS model
+dnn4.load_state_dict(torch.load("SU25_LWRS/models/dnn_4_l2e1_test.pt", map_location=device))
 
-    # Evaluating and printing results for LWRS
-    clean_acc = evaluate_clean(LWRS_model, test_loader)
-    adv_acc = evaluate_under_attack(LWRS_model, test_loader, epsilon, alpha, num_iter)
-    print(f"Accuracy of LWRS on clean data: {clean_acc:.4f}")
-    print(f"Accuracy of LWRS under PGD attack: {adv_acc:.4f}")
+# Evaluating and printing results for LWRS
+clean_acc = evaluate_clean(dnn4, test_loader)
+adv_acc = evaluate_under_l2(dnn4, test_loader, epsilon, alpha, num_iter)
+print(f"Accuracy of model on clean data: {clean_acc:.4f}")
+print(f"Accuracy of model under PGD attack: {adv_acc:.4f}")
