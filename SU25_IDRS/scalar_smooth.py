563a44df129c70992043f2a439e2331a83d9110d
bensager <129688968+bensager@users.noreply.github.com>
2025-08-14 16:58:42 -0700

    Updated smoothing function that works with the new scalar model.

---
+++ SU25_IDRS/scalar_smooth.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import numpy as np
+import time
+
+def scalar_smoothing(pretrained, sigma, X, n_samples=50, beta=10, p_min=1e-5):
+    device = X.device
+    batch_size = len(X)
+    num_classes = 10
+
+    scores = torch.zeros(batch_size, n_samples, num_classes, device=device)
+    probs = torch.zeros_like(scores)
+    yp = []
+
+    for n in range(batch_size): 
+        epsilon = torch.randn(n_samples, 784, device=device) 
+        epsilon = epsilon * torch.sqrt(sigma[n])  # Elementwise multipying by square root of diagonal elements
+        epsilon = epsilon.view(n_samples, 28, 28)
+
+        current_img = X[n].expand(n_samples, -1, -1)
+        scores[n] = torch.softmax(beta * pretrained(current_img + epsilon), dim=1)
+        probs[n] = (1 - 10 * p_min) * scores[n] + p_min
+
+    avg_probs = probs.mean(dim=1)
+
+    min_prob = avg_probs.min().item()
+    if min_prob < p_min:
+        print(f"Error: Minimum probability {min_prob} is less than p_min {p_min}.")
+
+    for n in range(batch_size):
+        yp.append(torch.argmax(avg_probs[n]).item())
+
+    g = torch.topk(avg_probs, 2)
+    return g, yp
